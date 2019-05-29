6f595018f7213d94139df658540919388bfb810e
John-Li <ivan.lishchinovskiy@gmail.com>
2019-05-29 17:34:50 +0300

    added excoveralls

--- mix.exs
+++ mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -33,7 +40,8 @@ defmodule Issues.MixProject do
       {:httpoison, "~> 1.0.0"},
       {:poison, "~> 3.1"},
       {:ex_doc, "~> 0.19"},
-      {:earmark, "~> 1.2.4"}
+      {:earmark, "~> 1.2.4"},
+      {:excoveralls, "~> 0.5.5", only: :test}
     ]
   end
 end
